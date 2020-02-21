ff8869a7d0edc11fa7d8ac9b133c21d96f355f8f
github username <github email address>
2020-02-21 14:03:49 -0500

    Changed database from sqlite to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_11_135157) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dinner_parties", force: :cascade do |t|
     t.integer "user_id"
     t.date "date"
